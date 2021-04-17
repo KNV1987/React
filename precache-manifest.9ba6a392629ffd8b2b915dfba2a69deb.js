b5b7d92bc13d432d102bd2a9eadb292b5db8e304
KNV 1987 <Kalibabchuk@yandex.ru>
2021-04-17 22:19:56 +0300

    Updates

---
+++ precache-manifest.9ba6a392629ffd8b2b915dfba2a69deb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e24dfb1cfc4ad5081d9606b1a59bb2a",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "f4a52a74676b659e08b8",
+    "url": "/React/static/css/3.23658cc5.chunk.css"
+  },
+  {
+    "revision": "56bde376727d27546b65",
+    "url": "/React/static/css/4.930b28e7.chunk.css"
+  },
+  {
+    "revision": "fa78aef5c87a69b27fc3",
+    "url": "/React/static/css/main.3bf7378f.chunk.css"
+  },
+  {
+    "revision": "13e0924d921782d951cf",
+    "url": "/React/static/js/2.82c33ed3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React/static/js/2.82c33ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a52a74676b659e08b8",
+    "url": "/React/static/js/3.2ee1f166.chunk.js"
+  },
+  {
+    "revision": "56bde376727d27546b65",
+    "url": "/React/static/js/4.a7351565.chunk.js"
+  },
+  {
+    "revision": "fa78aef5c87a69b27fc3",
+    "url": "/React/static/js/main.62fc2f34.chunk.js"
+  },
+  {
+    "revision": "a78e1d4fab5cc4664512",
+    "url": "/React/static/js/runtime-main.00b6fc2e.js"
+  },
+  {
+    "revision": "3f6fcd4c0a51655d392fe95a99c29140",
+    "url": "/React/static/media/user.3f6fcd4c.png"
+  }
+]);
